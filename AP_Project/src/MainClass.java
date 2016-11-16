ca7ac967c6265160e5a3fdf44433d68c1d8bbff3
innocent-rajan <rajan15161@iiitd.ac.in>
2016-11-16 15:38:04 +0530

    Working abit

--- AP_Project/src/MainClass.java
+++ AP_Project/src/MainClass.java
@@ -1,5 +1,33 @@
-import java.util.*; 
+//package AP_Project;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
 
 public class MainClass {
+
+    public static void main(String[] args) {
+    SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+    try {
+        SAXParser saxParser = saxParserFactory.newSAXParser();
+        //saxParser.setProperty("SP_GENERAL_ENTITY_SIZE_LIMIT",50000000);
+        parse handler = new parse();
+        saxParser.parse(new File("dblp.xml"), handler);
+        System.out.println("Starting.");
+        //Get Employees list
+        List<Record> empList = handler.getEmpList();
+        //print employee information
+        for(Record rec : empList)
+            System.out.println(rec);
+    } catch (ParserConfigurationException | SAXException | IOException e) {
+        e.printStackTrace();
+    }
+    }
 
 }
--- AP_Project/src/parse.java
+++ AP_Project/src/parse.java
@@ -1,4 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class parse {
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+public class parse extends DefaultHandler {
+	 private List<Record> empList = null;
+	    private Record rec = null;
+
+
+	    //getter method for Record list
+	    public List<Record> getEmpList() {
+	        return empList;
+	    }
+	    
+	    boolean bTitle = false;
+	    boolean bName = false;
+	    boolean bGender = false;
+	    boolean bRole = false;
+
+	    @Override
+	    public void startElement(String uri, String localName, String qName, Attributes attributes)
+	            throws SAXException {
+
+	        if (qName.equalsIgnoreCase("dblp")) {
+	            //create a new record and put it in Map
+	            String id = attributes.getValue("id");
+	            //initialize record object and set id attribute
+	            rec = new Record();
+	            //initialize list
+	            if (empList == null)
+	                empList = new ArrayList<>();
+	        } else if (qName.equalsIgnoreCase("article")) {
+	            //set boolean values for fields, will be used in setting record variables
+	            bName = true;
+	            System.out.println("Article");
+	        } else if (qName.equalsIgnoreCase("author")) {
+	            bTitle = true;
+	            System.out.println("Authors");
+	        } else if (qName.equalsIgnoreCase("pages")) {
+	            bGender = true;
+	            System.out.println("Pages");
+	        } else if (qName.equalsIgnoreCase("book")) {
+	            bRole = true;
+	            System.out.println("Book");
+	        }
+	    }
+
+	    @Override
+	    public void endElement(String uri, String localName, String qName) throws SAXException {
+	        if (qName.equalsIgnoreCase("record")) {
+	            //add record object to list
+	            empList.add(rec);
+	        }
+	    }
+
+	    @Override
+	    public void characters(char ch[], int start, int length) throws SAXException {
+
+	        if (bTitle) {
+	            //age element, set record age
+	            rec.setTitle((new String(ch, start, length)));
+	            bTitle = false;
+	        }
+	    }
 }
