53d66920d7054622a2f40e5129b9c5926a84c1f2
innocent-rajan <rajan15161@iiitd.ac.in>
2016-11-17 12:26:32 +0530

    Changes in mainclass file.

--- AP_Project/src/MainClass.java
+++ AP_Project/src/MainClass.java
@@ -13,13 +13,15 @@ import org.xml.sax.SAXException;
 public class MainClass {
 
     public static void main(String[] args) {
+    System.setProperty("jdk.xml.entityExpansionLimit", "0");
     SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
     try {
         SAXParser saxParser = saxParserFactory.newSAXParser();
         //saxParser.setProperty("SP_GENERAL_ENTITY_SIZE_LIMIT",50000000);
         parse handler = new parse();
-        saxParser.parse(new File("dblp.xml"), handler);
         System.out.println("Starting.");
+        saxParser.parse(new File("dblp.xml"), handler);
+        System.out.println("Ending.");
         //Get Employees list
         List<Record> empList = handler.getEmpList();
         //print employee information
