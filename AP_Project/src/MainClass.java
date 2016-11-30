a7eb74620d69d813182d79ae9af51252b7c45427
Rajan Girsa <rajan15161@iiitd.ac.in>
2016-11-30 05:39:14 +0530

    Add files via upload

--- AP_Project/src/MainClass.java
+++ AP_Project/src/MainClass.java
@@ -1,8 +1,8 @@
 //package AP_Project;
-
+//@author Rajan Girsa and Avdesh Kumar
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,43 +12,52 @@ import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.SAXException;
 
 public class MainClass {
-
-    public static void main(String[] args) {
+	static Query1 q1 = new Query1();
+	static Query2 q2 = new Query2();
+    @SuppressWarnings("resource")
+	public static void main(String[] args) {
+    	int size=0;
     	String aut=new String();
     	Scanner user_input = new Scanner( System.in );
+    	parseall();
 		System.out.print("Enter author : ");
 		aut=user_input.nextLine();
-		parse handler = new parse();
-		handler.setAuthor_s(aut);
-		System.setProperty("jdk.xml.entityExpansionLimit", "0");
+    	ArrayList<Authors> au=q1.getauthList();
+    	ArrayList<String> name = new ArrayList<String>();
+    	for(Authors Author : au) {
+    		for(int i=0;i<Author.getNames().size();++i){
+    			if(Author.getNames().get(i).equals(aut)){
+    				size=Author.getNames().size();
+    				for(int j=0;j<size;++j)
+        				name.add(Author.getNames().get(j));
+    			}
+    		}
+    	}
+    	System.out.print("Enter Title : ");
+    	String title=user_input.nextLine();
+    	q1.setTitle(title);
+    	q1.setAuthor_s(name);
+    	parseall();
+    	q1.sort();
+    	/*int size=0;
+    	String aut=new String();
+    	aut=null;
+    	parseall();
+    	q2.make_hash();
+       	//q1.setAuthor_s(name);
+    	//parseall();*/
+    	}
+
+    public static void parseall(){
+    	System.setProperty("jdk.xml.entityExpansionLimit", "0");
 		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 		try {
 			SAXParser saxParser = saxParserFactory.newSAXParser();
 			System.out.println("Starting.");
-			saxParser.parse(new File("dblp.xml"), handler);
+			saxParser.parse(new File("dblp.xml"), q1);
 			System.out.println("Ending.");
-        //List<Record> empList = handler.getRecList();
-        /*List<www> empList = handler.getauthList();
-        int i=0;
-        for(www www : empList) {
-        	System.out.println(www.toString());
-        	i++;
-        	if (i==100)
-        		break;
-        	}*/
     		} catch (ParserConfigurationException | SAXException | IOException e) {
     			e.printStackTrace();
-    		}	
     	}
-
-    /*public static void print(Record rec){
-    	//System.out.println("Month : " +rec.getMonth());
-    	System.out.println("Year : " +rec.getYear());
-    	System.out.println("Pages : " +rec.getPages());
-    	System.out.println("Title : " +rec.getTitle());
-    	System.out.println("Url : " +rec.getUrl());
-    	System.out.println("Volume : " +rec.getVolume());
-    	System.out.println("Authors : " +rec.getAuthors());
-    }*/
-    
+    }    
 }
